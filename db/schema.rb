698335be36b29b8da394821b83a44784eece7e26
Connor Peshek <connorpeshek@gmail.com>
2016-01-11 12:19:01 -0600

    Add user microposts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141111014949) do
+ActiveRecord::Schema.define(version: 20160111170830) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
