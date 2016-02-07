b3fcdd88ec9c3fd575c36236bd22ad69c9d59ad2
Connor Peshek <connorpeshek@gmail.com>
2016-02-07 12:45:31 -0600

    Changed title to Connor Peshek's Blog

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "Blog"
+    base_title = "Connor Peshek's Blog"
     if page_title.empty?
       base_title
     else
