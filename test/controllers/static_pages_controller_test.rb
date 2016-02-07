3f54d9a3e175b7caec3c36afdd8811adaee66453
Connor Peshek <connorpeshek@gmail.com>
2016-02-07 12:47:38 -0600

    Updated test to reflect title change

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,24 +5,24 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Blog"
+    assert_select "title", "Connor Peshek's Blog"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Blog"
+    assert_select "title", "Help | Connor Peshek's Blog"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Blog"
+    assert_select "title", "About | Connor Peshek's Blog"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "Contact | Blog"
+    assert_select "title", "Contact | Connor Peshek's Blog"
   end
 end
